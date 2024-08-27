8e37acaae84fee0aaaaf01a917be0f046afb0d60
Carlier Aurélien <carlier.aurelien28@gmail.com>
2024-08-27 15:36:02 +0200

    Update Streamlit_test.py

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -31,7 +31,7 @@ container.markdown(
     <div style="display: flex; align-items: center; justify-content: space-between;">
         <img src="{image}" style="width: 200px; margin-right: 10px;">
         <h1 style="color: #113f60; margin: 0;">La résidence secondaire à prix accessible !</h1>
-        <img src="{image_2}" style="width: 100px; margin-right: 10px; margin-bottom: 10px;">
+        <img src="{image_2}" style="width: 100px; margin-right: 10px; margin-bottom: 20px;">
     </div>
     ''',
     unsafe_allow_html=True
