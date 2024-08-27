b035a02b56caf4aa6670931a885287d5cd48ef7f
Carlier Aurélien <carlier.aurelien28@gmail.com>
2024-08-27 11:26:39 +0200

    Update Streamlit_test.py

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -4,7 +4,7 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
  
 # Backend
-#st.set_page_config(layout="wide", initial_sidebar_state='expanded')
+st.set_page_config(layout="wide", initial_sidebar_state='expanded')
 #credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
 #client = bigquery.Client(credentials=credentials)
 
