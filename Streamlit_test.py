0ecbaa4de992820a2cee54835f79ba5cea7a048e
Carlier Aurélien <carlier.aurelien28@gmail.com>
2024-08-27 14:53:31 +0200

    Update Streamlit_test.py

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -24,12 +24,14 @@ with open('style.css') as f:
 
 # Logo - Titre
 image = "https://i.goopics.net/an3xxk.png"
+image_2 = "https://etudestech.com/ecole/le-wagon/"
 container = st.container()
 container.markdown(
     f'''
     <div style="display: flex; align-items: center; justify-content: space-between;">
         <img src="{image}" style="width: 200px; margin-right: 10px;">
         <h1 style="color: #113f60; margin: 0;">La résidence secondaire à prix accessible !</h1>
+        <img style="display: flex; align-items: center; justify-content: space-between;">
     </div>
     ''',
     unsafe_allow_html=True
@@ -106,31 +108,80 @@ with col6:
        placeholder="Choix",
        key="Critere 6"
 )
-geodata = gpd.read_file('data_geo/departements.geojson')
-df_geo = pd.merge(geodata, top_10, left_on='code', right_on='department_code', how='inner')
-df_geo = df_geo.drop(['code', 'nom'], axis=1)
+ # Mapping
+#geodata = gpd.read_file('data_geo/departements.geojson')
+#df_geo = pd.merge(geodata, top_10, left_on='code', right_on='department_code', how='inner')
+#df_geo = df_geo.drop(['code', 'nom'], axis=1)
 
 # Création d'une map "choropleth", elle affichera les 10 départements selectionnés.
-       fig = px.choropleth_mapbox(df_geo, 
-                               geojson=df_geo.geometry, 
-                               locations=df_geo.index,  
-                               mapbox_style="carto-positron", 
-                               hover_name='department_name',
-                               color= "colorank",  
-                               color_discrete_map=color_dict, 
-                               center={ "lat": 46.8, "lon": 1.8}, 
-                               custom_data=['department_name', 'geographie'], 
-                               zoom=4.2, 
-                               opacity=0.9) 
+       #fig = px.choropleth_mapbox(df_geo, 
+                               #geojson=df_geo.geometry, 
+                               #locations=df_geo.index,  
+                               #mapbox_style="carto-positron", 
+                               #hover_name='department_name',
+                               #color= "colorank",  
+                               #color_discrete_map=color_dict, 
+                               #center={ "lat": 46.8, "lon": 1.8}, 
+                               #custom_data=['department_name', 'geographie'], 
+                               #zoom=4.2, 
+                               #opacity=0.9) 
                                 
 
-       fig.update_traces(
-       hovertemplate=
-           "<b>%{customdata[0]}</b><br>Relief: %{customdata[1]}"
-       )
+       #fig.update_traces(
+       #hovertemplate=
+           #"<b>%{customdata[0]}</b><br>Relief: %{customdata[1]}"
+       #)
 
-       fig.update_layout(margin={"r": 0, "t": 10, "l": 0, "b": 0}, showlegend=False, legend_itemwidth=35, width=650)
-       col1.plotly_chart(fig, use_container_width=True)
+       #fig.update_layout(margin={"r": 0, "t": 10, "l": 0, "b": 0}, showlegend=False, legend_itemwidth=35, width=650)
+       #col1.plotly_chart(fig, use_container_width=True)
+
+
+
+# Graph tableau
+    #with col2: 
+
+            #top_10["avg_sales_maison"] = top_10["avg_sales_maison"].round(decimals=0)
+            #top_10["avg_sales_maison"] = top_10["avg_sales_maison"].astype(int)
+            #top_10["avg_sales_appt"] = top_10["avg_sales_appt"].round(decimals=0)
+            #top_10["avg_sales_appt"] = top_10["avg_sales_appt"].astype(int)
+
+            #def add_euro_symbol(value):
+                #return f"{value} €"
+
+            #top_10["avg_sales_maison"] = top_10["avg_sales_maison"].map(add_euro_symbol)
+            #top_10["avg_sales_appt"] = top_10["avg_sales_appt"].map(add_euro_symbol)
+
+            #top_10_renamed = top_10[["department_code", "department_name", "avg_sales_maison", "avg_sales_appt"]].rename(
+                    #columns={"department_code": "Numéros", "department_name": "Départements", "avg_sales_maison":"Prix moyen d'une maison",
+                             #"avg_sales_appt":"Prix moyen d'un appart"})
+
+           # top_10_no_index = top_10_renamed.reset_index(drop=True)
+           # table_html = top_10_no_index.to_html(index=False)
+
+            #style_css = """
+                #<style>
+               # th {
+                   # background-color: #E1DCCA; 
+               # }
+               # th:nth-child(1) {
+                #text-align: center; 
+                #  }
+                #th:nth-child(2) {
+                #text-align: center;
+                #}
+               # }
+               # th:nth-child(3) {
+                #text-align: center;
+               # }
+                #}
+                #th:nth-child(4) {
+                #text-align: center;
+                #}
+                #td {
+                #text-align: center; 
+                #}
+                #</style>
+            #"""
  
 
 
