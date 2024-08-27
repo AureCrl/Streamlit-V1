11f2a661af5d967a077bbc1f05db972e15d36632
Carlier Aurélien <carlier.aurelien28@gmail.com>
2024-08-27 14:10:27 +0200

    Update Streamlit_test.py

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -19,11 +19,11 @@ with open('style.css') as f:
 
 # Logo - Titre
 image = "https://i.goopics.net/an3xxk.png"
-container = st.container()
+# Centrer le titre et le logo
 container.markdown(
     f'''
-    <div style="display: flex; align-items: center; justify-content: space-between;">
-        <img src="{image}" style="width: 200px; margin-right: 10px;">
+    <div style="text-align: center; margin-bottom: 10px;">
+        <img src="{image}" style="width: 200px; margin-bottom: 10px;">
         <h1 style="color: #113f60; margin: 0;">La résidence secondaire à prix accessible !</h1>
     </div>
     ''',
