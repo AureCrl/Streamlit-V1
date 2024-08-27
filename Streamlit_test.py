697de36250f1a3ab0847218e623640a023aac870
Carlier Aurélien <carlier.aurelien28@gmail.com>
2024-08-27 10:21:36 +0200

    Update Streamlit_test.py

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -1,7 +1,30 @@
 import streamlit as st
 import pandas as pd
+from google.oauth2 import service_account
+from google.cloud import bigquery
  
+
+#st.set_page_config(layout="wide", initial_sidebar_state='expanded')
+#credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+#client = bigquery.Client(credentials=credentials)
+
+#with open('style.css') as f:
+ #   st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+#query = ("SELECT * FROM `projet-prello.transform_prello.score_table_full` ")
+
+#query_job = client.query(query)
+#query_result = query_job.result()
+#data = query_result.to_dataframe()
 st.write("""
 # My first app
 Hello *world!*
 """)
+image = "https://i.goopics.net/an3xxk.png"
+container = st.container()
+container.markdown(
+        f'<div style="display: flex; justify-content: center; margin-top: 0px; margin-bot: 0px;">'
+        f'<img src="{image}" style="width: 5;">'
+        f'</div>',
+        unsafe_allow_html=True
+    )
