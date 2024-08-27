95047498644c8ae25504c6f583860cfc810b1757
Carlier Aurélien <carlier.aurelien28@gmail.com>
2024-08-27 12:07:13 +0200

    Update Streamlit_test.py

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -62,6 +62,7 @@ Critere_6 = False
 
 # Critere 1
 critere_1 = st.selectbox(
+     "How would you like to be contacted?",
     ("Email", "Home phone", "Mobile phone"),
  index=None,
     placeholder="Choix",
