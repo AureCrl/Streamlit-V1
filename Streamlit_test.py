11115b96f2bd43feb72659fc3d8272d15a150959
Carlier Aurélien <carlier.aurelien28@gmail.com>
2024-08-29 10:02:20 +0200

    Update Streamlit_test.py

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -11,6 +11,9 @@ import plotly.graph_objects as go
 data = pd.read_csv("merged_df_streamlit.csv")
 geojson_data = gpd.read_file("departements.geojson")
 
+data['code'] = data['code'].astype(str)
+geojson_data['code'] = geojson_data['code'].astype(str)
+
 # Backend
 st.set_page_config(layout="wide", initial_sidebar_state='expanded')
 with open('style.css') as f:
